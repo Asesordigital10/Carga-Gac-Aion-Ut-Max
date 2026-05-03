d6ad543716cba2c36aa98491cdfc59ddffe9953a
RECEPCIONISTA DIGITAL <recepcionistadigitaluy@gmail.com>
2026-05-02 21:22:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const CACHE_NAME = 'asysauto-v5';
+const CACHE_NAME = 'asysauto-v6'; // Cambiado a v6 para forzar descarga
 const assets = ['./', './index.html', './logoasys.png', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); // Obliga al nuevo SW a tomar el control inmediatamente
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
     })
   );
 });
